8f3384a2246823901895be8bc5f19ea8028a1b6b
daniel doe <danieldoe33@gmail.com>
2019-05-02 18:33:28 +0100

    sample pages to route to

---
+++ renderer/components/dashboard/routePages.js
@@ -0,0 +1,17 @@
+export const First = () => {
+    return (
+        <div>This is the first page</div>
+    );
+}
+
+export const Second = () => {
+    return (
+        <div>This is the Second page</div>
+    );
+}
+
+export const Third = () => {
+    return (
+        <div>This is the third page</div>
+    );
+}
