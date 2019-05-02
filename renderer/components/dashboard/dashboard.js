29b466fa3e3a74f5ec3ef23604262cc4935833d9
daniel doe <danieldoe33@gmail.com>
2019-05-02 18:34:07 +0100

    added main dashboard code

--- renderer/components/dashboard/dashboard.js
+++ renderer/components/dashboard/dashboard.js
@@ -1,184 +1,7 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import classNames from 'classnames';
-import { withStyles } from '@material-ui/core/styles';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import Drawer from '@material-ui/core/Drawer';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import List from '@material-ui/core/List';
-import Typography from '@material-ui/core/Typography';
-import Divider from '@material-ui/core/Divider';
-import IconButton from '@material-ui/core/IconButton';
-import Badge from '@material-ui/core/Badge';
-import MenuIcon from '@material-ui/icons/Menu';
-import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
-import NotificationsIcon from '@material-ui/icons/Notifications';
-import { mainListItems, secondaryListItems } from './listItems';
-
-const drawerWidth = 240;
-
-const styles = theme => ({
-  root: {
-    display: 'flex',
-  },
-  toolbar: {
-    paddingRight: 24, // keep right padding when drawer closed
-  },
-  toolbarIcon: {
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'flex-end',
-    padding: '0 8px',
-    ...theme.mixins.toolbar,
-  },
-  appBar: {
-    zIndex: theme.zIndex.drawer + 1,
-    transition: theme.transitions.create(['width', 'margin'], {
-      easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.leavingScreen,
-    }),
-  },
-  appBarShift: {
-    marginLeft: drawerWidth,
-    width: `calc(100% - ${drawerWidth}px)`,
-    transition: theme.transitions.create(['width', 'margin'], {
-      easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.enteringScreen,
-    }),
-  },
-  menuButton: {
-    marginLeft: 12,
-    marginRight: 36,
-  },
-  menuButtonHidden: {
-    display: 'none',
-  },
-  title: {
-    flexGrow: 1,
-  },
-  drawerPaper: {
-    position: 'relative',
-    whiteSpace: 'nowrap',
-    width: drawerWidth,
-    transition: theme.transitions.create('width', {
-      easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.enteringScreen,
-    }),
-  },
-  drawerPaperClose: {
-    overflowX: 'hidden',
-    transition: theme.transitions.create('width', {
-      easing: theme.transitions.easing.sharp,
-      duration: theme.transitions.duration.leavingScreen,
-    }),
-    width: theme.spacing.unit * 7,
-    [theme.breakpoints.up('sm')]: {
-      width: theme.spacing.unit * 9,
-    },
-  },
-  appBarSpacer: theme.mixins.toolbar,
-  content: {
-    flexGrow: 1,
-    padding: theme.spacing.unit * 3,
-    height: '100vh',
-    overflow: 'auto',
-  },
-  chartContainer: {
-    marginLeft: -22,
-  },
-  tableContainer: {
-    height: 320,
-  },
-  h5: {
-    marginBottom: theme.spacing.unit * 2,
-  },
-});
-
-class Dashboard extends React.Component {
-  state = {
-    open: true,
-  };
-
-  handleDrawerOpen = () => {
-    this.setState({ open: true });
-  };
-
-  handleDrawerClose = () => {
-    this.setState({ open: false });
-  };
-
-  render() {
-    const { classes } = this.props;
-
-    return (
-      <div className={classes.root}>
-        <CssBaseline />
-        <AppBar
-          position="absolute"
-          className={classNames(classes.appBar, this.state.open && classes.appBarShift)}
-        >
-          <Toolbar disableGutters={!this.state.open} className={classes.toolbar}>
-            <IconButton
-              color="inherit"
-              aria-label="Open drawer"
-              onClick={this.handleDrawerOpen}
-              className={classNames(
-                classes.menuButton,
-                this.state.open && classes.menuButtonHidden,
-              )}
-            >
-              <MenuIcon />
-            </IconButton>
-            <Typography
-              component="h1"
-              variant="h6"
-              color="inherit"
-              noWrap
-              className={classes.title}
-            >
-              Dashboard
-            </Typography>
-            <IconButton color="inherit">
-              <Badge badgeContent={4} color="secondary">
-                <NotificationsIcon />
-              </Badge>
-            </IconButton>
-          </Toolbar>
-        </AppBar>
-        <Drawer
-          variant="permanent"
-          classes={{
-            paper: classNames(classes.drawerPaper, !this.state.open && classes.drawerPaperClose),
-          }}
-          open={this.state.open}
-        >
-          <div className={classes.toolbarIcon}>
-            <IconButton onClick={this.handleDrawerClose}>
-              <ChevronLeftIcon />
-            </IconButton>
-          </div>
-          <Divider />
-          <List>{mainListItems}</List>
-          <Divider />
-          <List>{secondaryListItems}</List>
-        </Drawer>
-        <main className={classes.content}>
-          <div className={classes.appBarSpacer} />
-
-        </main>
-      </div>
-    );
-  }
-}
-
-Dashboard.propTypes = {
-  classes: PropTypes.object.isRequired,
-};
-
-export default withStyles(styles)(Dashboard);
-
+// import React from 'react';
 // import { Layout, Menu, Icon } from "antd";
+// import { Link } from 'react-router-dom';
+// import { First, Second, Third } from './routePages';
 // import "./dashboard.css";
 
 // const { Header, Sider, Content } = Layout;
@@ -194,6 +17,15 @@ export default withStyles(styles)(Dashboard);
 //     });
 //   };
 
+//   	renderContent(){
+// 		switch (this.props.location.pathname) {
+// 			case '/first':
+// 				return <First/>
+// 			case '/second':
+// 				return <Second />
+// 		}
+// 	}
+
 //   render() {
 //     return (
 //       <div id="#components-layout-demo-custom-trigger">
@@ -203,11 +35,11 @@ export default withStyles(styles)(Dashboard);
 //             <Menu theme="dark" mode="inline" defaultSelectedKeys={["1"]}>
 //               <Menu.Item key="1">
 //                 <Icon type="user" />
-//                 <span>nav 1</span>
+//                 <Link to="/first">First</Link>
 //               </Menu.Item>
 //               <Menu.Item key="2">
 //                 <Icon type="video-camera" />
-//                 <span>nav 2</span>
+//                 <Link to="/second"><span>Second</span></Link>
 //               </Menu.Item>
 //               <Menu.Item key="3">
 //                 <Icon type="upload" />
@@ -231,7 +63,7 @@ export default withStyles(styles)(Dashboard);
 //                 minHeight: 280,
 //               }}
 //             >
-//               Content
+//               {this.renderContent()}
 //             </Content>
 //           </Layout>
 //         </Layout>
@@ -239,3 +71,207 @@ export default withStyles(styles)(Dashboard);
 //     );
 //   }
 // }
+
+import React from 'react';
+import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import classNames from 'classnames';
+import { withStyles } from '@material-ui/core/styles';
+import Drawer from '@material-ui/core/Drawer';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import List from '@material-ui/core/List';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Typography from '@material-ui/core/Typography';
+import Divider from '@material-ui/core/Divider';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
+import ChevronRightIcon from '@material-ui/icons/ChevronRight';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import InboxIcon from '@material-ui/icons/MoveToInbox';
+import MailIcon from '@material-ui/icons/Mail';
+import Paper from '@material-ui/core/Paper';
+import { First, Second, Third } from './routePages';
+
+const drawerWidth = 240;
+
+const styles = theme => ({
+	root: {
+		display: 'flex',
+		width: "100%",
+		height: "100vh"
+	},
+	appBar: {
+		zIndex: theme.zIndex.drawer + 1,
+		transition: theme.transitions.create(['width', 'margin'], {
+			easing: theme.transitions.easing.sharp,
+			duration: theme.transitions.duration.leavingScreen,
+		}),
+	},
+	appBarShift: {
+		marginLeft: drawerWidth,
+		width: `calc(100% - ${drawerWidth}px)`,
+		transition: theme.transitions.create(['width', 'margin'], {
+			easing: theme.transitions.easing.sharp,
+			duration: theme.transitions.duration.enteringScreen,
+		}),
+	},
+	menuButton: {
+		marginLeft: 12,
+		marginRight: 36,
+	},
+	hide: {
+		display: 'none',
+	},
+	drawer: {
+		width: drawerWidth,
+		flexShrink: 0,
+		whiteSpace: 'nowrap',
+	},
+	drawerOpen: {
+		width: drawerWidth,
+		transition: theme.transitions.create('width', {
+			easing: theme.transitions.easing.sharp,
+			duration: theme.transitions.duration.enteringScreen,
+		}),
+	},
+	drawerClose: {
+		transition: theme.transitions.create('width', {
+			easing: theme.transitions.easing.sharp,
+			duration: theme.transitions.duration.leavingScreen,
+		}),
+		overflowX: 'hidden',
+		width: theme.spacing.unit * 7 + 1,
+		[theme.breakpoints.up('sm')]: {
+			width: theme.spacing.unit * 9 + 1,
+		},
+	},
+	toolbar: {
+		display: 'flex',
+		alignItems: 'center',
+		justifyContent: 'flex-end',
+		padding: '0 8px',
+		...theme.mixins.toolbar,
+	},
+	content: {
+		flexGrow: 1,
+		padding: theme.spacing.unit * 3,
+	},
+	sidemenu: {
+		...theme.mixins.gutters(),
+    paddingTop: theme.spacing.unit * 2,
+	paddingBottom: theme.spacing.unit * 2,
+	height: "90%",
+	}
+});
+
+class Dashboard extends React.Component {
+	state = {
+		open: false,
+	};
+
+	handleDrawerOpen = () => {
+		this.setState({ open: true });
+	};
+
+	handleDrawerClose = () => {
+		this.setState({ open: false });
+	};
+
+	renderContent() {
+		switch (this.props.location.pathname) {
+			case '/first':
+				return <First />;
+			case '/second':
+				return <Second />;
+			case '/third':
+				return <Third />;
+		}
+	}
+
+	render() {
+		const { classes, theme, routes, sub_routes } = this.props;
+
+		return (
+			<div className={classes.root}>
+				<CssBaseline />
+				<AppBar
+					position="fixed"
+					className={classNames(classes.appBar, {
+						[classes.appBarShift]: this.state.open,
+					})}
+				>
+					<Toolbar disableGutters={!this.state.open}>
+						<IconButton
+							color="inherit"
+							aria-label="Open drawer"
+							onClick={this.handleDrawerOpen}
+							className={classNames(classes.menuButton, {
+								[classes.hide]: this.state.open,
+							})}
+						>
+							<MenuIcon />
+						</IconButton>
+						<Typography variant="h6" color="inherit" noWrap>
+							Dashboard
+						</Typography>
+					</Toolbar>
+				</AppBar>
+				<Drawer
+					variant="permanent"
+					className={classNames(classes.drawer, {
+						[classes.drawerOpen]: this.state.open,
+						[classes.drawerClose]: !this.state.open,
+					})}
+					classes={{
+						paper: classNames({
+							[classes.drawerOpen]: this.state.open,
+							[classes.drawerClose]: !this.state.open,
+						}),
+					}}
+					open={this.state.open}
+				>
+					<div className={classes.toolbar}>
+						<IconButton onClick={this.handleDrawerClose}>
+							{theme.direction === 'rtl' ? <ChevronRightIcon /> : <ChevronLeftIcon />}
+						</IconButton>
+					</div>
+					<Divider />
+					<List>
+						{routes.map((elem, index) => (
+							<ListItem button key={elem.key} component={Link} to={elem.path} selected={elem.path===this.props.location.pathname}>
+								<ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
+								<ListItemText primary={elem.name} />
+							</ListItem>
+						))}
+					</List>
+					<Divider />
+					<List>
+						{sub_routes.map((elem, index) => (
+							<ListItem button key={elem.key} component={Link} to={elem.path} selected={elem.path===this.props.location.pathname}>
+								<ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
+								<ListItemText primary={elem.name} />
+							</ListItem>
+						))}
+					</List>
+				</Drawer>
+				<main className={classes.content}>
+					<div className={classes.toolbar} />
+						<Paper className={classes.sidemenu} elevation={1}>	
+							{this.renderContent()}
+						</Paper>	
+				</main>
+			</div>
+		);
+	}
+}
+
+Dashboard.propTypes = {
+	classes: PropTypes.object.isRequired,
+	theme: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles, { withTheme: true })(Dashboard);
